55485efccc9ba33959a6d14f34c0f5552d87a998
arselane <arselanearis9@gmail.com>
2026-09-02 15:58:16 +0100

    trainings page + whatsapp

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -51,16 +51,15 @@
     }
     .sidenav {
         padding: 50px;
-  height: 100%; /* Full-height: remove this if you want "auto" height */
-  width: 350px; /* Set the width of the sidebar */
-  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
-  z-index: 1; /* Stay on top */
+  height: 100%; 
+  width: 350px;
+  position: fixed; 
+  z-index: 1;
   left: 0;
-  overflow-x: hidden; /* Disable horizontal scroll */
+  overflow-x: hidden; 
   padding-top: 20px;
 }
 
-/* The navigation menu links */
 .sidenav a {
   padding: 6px 8px 6px 16px;
   text-decoration: none;
@@ -69,14 +68,12 @@
   display: block;
 }
 
-/* When you mouse over the navigation links, change their color */
 .sidenav a:hover {
   color: black;
 }
 
-/* Style page content */
 .main {
-  margin-left: 300px; /* Same as the width of the sidebar */
+  margin-left: 300px;
   padding: 50px;
 }
 .main input{
@@ -85,7 +82,6 @@
     border-radius: 5px;
 }
 
-/* On smaller screens, where height is less than 450px, change the style of the sidebar (less padding and a smaller font size) */
 @media screen and (max-height: 450px) {
   .sidenav {padding-top: 15px;}
   .sidenav a {font-size: 18px;}
