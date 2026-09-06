d383b7c58c164995610fcc3e6c3328ed06cf7fe6
arselane <arselanearis9@gmail.com>
2026-09-06 17:13:43 +0100

    fixed trainings page

--- resources/views/components/sidebar/sidebar.blade.php
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -1,4 +1,127 @@
-<div class="sidenav">
+<section class="trainings">
+    <input type="text" placeholder="Search for a training">
+    <div class="trainings-items">
+        <h1>Categories</h1>
+        <span>All Trainings</span>
+        <span>Cisco Systems</span>
+        <span>CompTIA</span>
+        <span>Web development</span>
+        <span>Microsoft Azure</span>
+        <span>Cybersecurity</span>
+        <span>Web Development</span>
+        <span>Artificial Intelligence</span>
+        <span>Telecommunications</span>
+        <span>Security Systems</span>
+    </div><br>
+    <span class="count"># trainings found</span>
+    <div class="popular-info trainings-popular">
+        <div class="popular-info-item">
+            <img class="popular-item" src="{{ asset('images/popular-image.png') }}" alt="">
+            <span class="popular-sticky-item">cisco</span>
+            <h3>Getting Started with shadcn/ui: A Complete Guide</h3>
+            <p>Whether you want to learn fundamental skills, boost your career with an international certification, or change your career.</p>
+            <span class="duration"><i class="bi bi-clock"></i> Duration: 7 days</span><br>
+            <a href="{{ url('/') }}">Learn more <i class="bi bi-chevron-right"></i></a>
+        </div>
+        <div class="popular-info-item">
+            <img class="popular-item" src="{{ asset('images/popular-image.png') }}" alt="">
+            <span class="popular-sticky-item">cisco</span>
+            <h3>Getting Started with shadcn/ui: A Complete Guide</h3>
+            <p>Whether you want to learn fundamental skills, boost your career with an international certification, or change your career.</p>
+            <span class="duration"><i class="bi bi-clock"></i> Duration: 7 days</span><br>
+            <a href="{{ url('/') }}">Learn more <i class="bi bi-chevron-right"></i></a>
+        </div>
+        <div class="popular-info-item">
+            <img class="popular-item" src="{{ asset('images/popular-image.png') }}" alt="">
+            <span class="popular-sticky-item">cisco</span>
+            <h3>Getting Started with shadcn/ui: A Complete Guide</h3>
+            <p>Whether you want to learn fundamental skills, boost your career with an international certification, or change your career.</p>
+            <span class="duration"><i class="bi bi-clock"></i> Duration: 7 days</span><br>
+            <a href="{{ url('/') }}">Learn more <i class="bi bi-chevron-right"></i></a>
+        </div>
+
+    </div>
+    
+</section>
+
+<style>
+    .trainings {
+        padding: 20px;
+        h1{
+            display: none;
+        }
+
+        input {
+            border: 1px solid grey;
+            padding: 10px;
+            border-radius: 10px;
+            width: 90vw;
+        }
+    }
+
+.trainings-items {
+        display: flex;
+        gap: 12px 20px; /* row-gap column-gap */
+        font-size: 13.5px;
+        margin-top: 20px;
+        overflow-x: auto;    /* scrolls down if there are more tags than fit */
+    }
+
+    .trainings-items > span {
+        flex: 0 0 auto;
+        white-space: nowrap;
+    }
+
+    @media (max-width:770px){
+        .trainings-popular{
+        flex-direction: column!important;
+    }
+    }
+    @media (min-width:771px){
+        .trainings-items{
+            margin-top: -65px;
+            display: flex;
+            flex-direction: column;
+                padding: 50px;
+                height: 100%; 
+                width: 350px;
+                position: absolute; 
+                z-index: 1;
+                left: 0;
+                overflow-x: hidden; 
+                padding-top: 20px;
+        }
+        .trainings-popular{
+            margin-left: 25%;
+
+        }
+        .trainings {
+        padding: 20px;
+        h1{
+            display: block;
+            font-size: large;
+            font-weight: 600;
+        }
+
+        input {
+            border: 1px solid grey;
+            padding: 10px;
+            border-radius: 10px;
+            max-width: 40vw;
+            margin-left: 25%;
+        }
+        .count{
+            margin-left: 25%;
+        }
+    }
+    
+    }
+    
+
+</style>
+
+
+{{-- <div class="sidenav">
     <h2>Categories</h2>
     <a href="#">All Trainings</a>
     <a href="#">Cisco Systems</a>
@@ -86,4 +209,4 @@
   .sidenav {padding-top: 15px;}
   .sidenav a {font-size: 18px;}
 }
-</style>
+</style> --}}
