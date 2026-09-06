465867f01b66e193f7e70d7bbead0ab92427e926
arselane <arselanearis9@gmail.com>
2026-09-06 11:20:00 +0100

    Added SEO META TAGS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    {{-- SEO --}}
+    <meta name="description" content="Etilec designs, builds, and supports digital products - MVPs, SaaS, and custom software - with fast, reliable development and support.">    
+    <meta name="copyright" content="Etilec">
+    <!-- <meta name="author" content="Badni Abdelwahab" /> -->
+    <meta name="keywords" content="Agence, Boite Developpement, entreprise, Estimation & Conseil de Projet, création site web, application web, application Mobile, Flutter, wordpress, Vuejs, Laravel, design moderne, Ui/Ux Algérie">
+    <link rel="canonical" href="/" />
+    <link rel="alternate" hreflang="en" href="/" />
+    <link rel="alternate" hreflang="fr" href="/fr" />
+
+    <!--  for indexing the page from the serchr engin -->
+    <meta name="robots" content="index, follow" />
+
+    <!-- for date of content -->
+    <meta name="revised" content="Tuesday, July 20th, 2019, 10:15 am" /> 
+
+    <!-- for social media websit -->
+    <meta name="og:title" content="Etilec" />
+    <meta name="og:type" content="teaching" />
+    <meta name="og:image" content="assets/media/logo-image.png" />
+    <meta name="og:description" content="Description About The Algorithm Article" />
+    
+
+
+
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
